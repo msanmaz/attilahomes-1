81946d1aa76eb4c18b96b7c3642529c04b985432
mosanmaz <mert.osanmaz@intercom.io>
2026-06-01 01:57:16 +0100

    Revert "add Google Search Console verification meta tag"
    This reverts commit e5894fd92ea035579f31a9e784392a6a3aaba377.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -47,7 +47,6 @@ export const metadata: Metadata = {
   other: {
     "apple-mobile-web-app-capable": "yes",
     "apple-mobile-web-app-status-bar-style": "black-translucent",
-    "google-site-verification": "FG2N5eNmdFyb7AeFBI24vc6lAwaRyoGNtnmrfs_o20k",
   },
 };
 
